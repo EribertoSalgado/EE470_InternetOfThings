4ac48a2e5531e20f9192895e80b2cf3689328377
EribertoSalgado <salgadoe@sonoma.edu>
2024-11-20 14:44:30 -0800

    Create firstThingSpeak.php

---
+++ RaspberryPi3_Thingspeak/firstThingSpeak.php
@@ -0,0 +1,24 @@
+<html>    
+<head><title> myThingsSpeak </title>    
+<!--****** Go to  http://web.sonoma.edu/users/f/farahman/subpages/junk/junk2.html -->    
+</head>    
+<body>    
+<h1>Welcome to My Webpage!</h1>  
+<p>Here is the data directly from the PI</p>  
+<table width="200" border="1">  
+  <tbody>  
+    <tr>  
+      <th scope="row"><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2755734/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+</th>  
+      <td><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2755734/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+</td>  
+    </tr>  
+  </tbody>  
+</table>  
+<p> </p>  
+</body> 
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2755734/maps/channel_show"></iframe>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2755734/widgets/973845"></iframe>
+<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/apps/matlab_visualizations/591561"></iframe>
+
+</html>
